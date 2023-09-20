8d0e84117fc9eaf015950bb8b8773a6cf12e44d3
AdrianvEik <85258266+AdrianvEik@users.noreply.github.com>
2023-09-20 21:05:19 +0200

    Added a "search_for" parameter in the load_from_directory method to target a single (or multiple) file types to combine into a single data set.

--- main.py
+++ main.py
@@ -19,17 +19,17 @@ class Model:
                 datlist.append(Datfile(i, format="x z", **{"y": location[i]}))
             self.model = model_constructor(datlist)
 
-    def load_from_directory(self, path: str):
-        # Locate all .dat files in the directory
-
+    def load_from_directory(self, path: str, search_for: str = ".dat"):
+        # Locate all .dat, .csv or .txt files in the directory
         datlist = []
         for i in os.listdir(path):
-            if i.endswith(".dat"):
+            if i.endswith(search_for):
                 if len(self.locations) > 0:
                     # If the location is specified, add it to the datfile
                     datlist.append(Datfile(path + "/" + i, format="x z", y=self.locations[i]))
                 else:
                     # If the location is not specified, it must be in the datfile or throw an error
                     datlist.append(Datfile(path + "/" + i, format="x z"))
+
         return datlist
 
